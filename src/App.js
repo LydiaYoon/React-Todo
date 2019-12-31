867c1c37c700239fe394fff7cc3c86daf0880715
N19144 <narae4096@interpark.com>
2019-12-31 14:42:25 +0900

    #5.1 예제 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
 import React from "react";
 // import React, { Component } from "react";
-import MyComponent from "./MyComponent";
-import Counter from "./Counter";
-import Say from "./Say";
-import EventPractice3 from "./EventPractice3";
+
+// import MyComponent from "./practice/MyComponent";
+// import Counter from "./practice/Counter";
+// import Say from "./practice/Say";
+import EventPractice3 from "./practice/EventPractice3";
+import ValidationSample from "./practice/ValidationSample";
 
 const App = () => {
   return (
     <>
-      <MyComponent name="메타몽" favoriteNumber={1}>
+      {/* <MyComponent name="메타몽" favoriteNumber={1}>
         리액트
-      </MyComponent>
-      <Counter />
-      <Say />
+      </MyComponent> */}
+      {/* <Counter /> */}
+      {/* <Say /> */}
       <EventPractice3 />
+      <ValidationSample />
     </>
   );
 };
-
-/*
-// 함수형 컴포넌트
-function App() {
-  const name = "react";
-  return <div className="react">{name === "react" && <h1>react</h1>}</div>;
-}
-
-// 클래스형 컴포넌트
-class App extends Component {
-  render() {
-    const name = "react";
-    return <div className="react">{name}</div>;
-  }
-}
-*/
 
 export default App;
---
+++ src/practice/ValidationSample.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./ValidationSample.css";
+
+class ValidationSample extends Component {
+  state = {
+    password: "",
+    clickec: false,
+    validated: false
+  };
+
+  handleChange = e => {
+    this.setState({
+      password: e.target.value
+    });
+  };
+
+  handleButtonClick = () => {
+    this.setState({
+      clicked: true,
+      validated: this.state.password === "0000"
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h4>Validation Sample</h4>
+        <input
+          type="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          className={
+            this.state.clicked
+              ? ( this.state.validated
+                ? "success"
+                : "failure" )
+              : ""
+          }
+        />
+        <button onClick={this.handleButtonClick}>Check</button>
+      </div>
+    );
+  }
+}
+
+export default ValidationSample;
