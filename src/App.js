34a21e2e0e560be8530f61aea8ec04c97327812c
N19144 <narae4096@interpark.com>
2020-01-06 17:25:55 +0900

    #7.3.3 에러 잡아내기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import LifeCycleSample from "./practice/LifeCycleSample";
+import ErrorBoundary from "./practice/ErrorBoundary";
 
 function getRandomColor() {
   return "#" + Math.floor(Math.random() * 16777215).toString(16);
@@ -21,7 +22,9 @@ class App extends Component {
     return (
       <div>
         <button onClick={this.handleClick}>random color</button>
-        <LifeCycleSample color={this.state.color} />
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </div>
     );
   }
