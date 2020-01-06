719c9162f86a7150a31af5d2d9850cb915eb8592
LydiaYoon <narae456@gmail.com>
2020-01-06 18:43:07 +0900

    #8.3 useReducer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React, { useState } from "react";
 
-import Info from "./practice/Info";
+import CounterReducer from "./practice/CounterReducer";
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-
   return (
     <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
+      <CounterReducer />
     </div>
   );
 };
